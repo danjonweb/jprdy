ff6c841f98a6c52c650fa63bb2a9f9d0f25456fc
danjonweb <dan.j.weber@gmail.com>
2014-08-20 10:38:37 -0700

    re-structured

--- js/events.js
+++ js/events.js
@@ -65,14 +65,22 @@ $('#level-select').change(function(){
 });
 
 $('#edit-teams').on('click',function(){
+	var colorSelectHTML = '<select name="color"><option value="#ac725e">#ac725e</option><option value="#d06b64">#d06b64</option><option value="#f83a22">#f83a22</option><option value="#fa573c">#fa573c</option><option value="#ff7537">#ff7537</option><option value="#ffad46">#ffad46</option><option value="#42d692">#42d692</option><option value="#16a765">#16a765</option><option value="#7bd148">#7bd148</option><option value="#b3dc6c">#b3dc6c</option><option value="#fbe983">#fbe983</option><option value="#fad165">#fad165</option><option value="#92e1c0">#92e1c0</option><option value="#9fe1e7">#9fe1e7</option><option value="#9fc6e7">#9fc6e7</option><option value="#4986e7">#4986e7</option><option value="#9a9cff">#9a9cff</option><option value="#b99aff">#b99aff</option><option value="#c2c2c2">#c2c2c2</option><option value="#cabdbf">#cabdbf</option><option value="#cca6ac">#cca6ac</option><option value="#f691b2">#f691b2</option><option value="#cd74e6">#cd74e6</option><option value="#a47ae2">#a47ae2</option></select>';
     var nodes = document.querySelectorAll('.team-name');
     var firstElement;
     for (var i = 0; i < nodes.length; i++) {
+    	var colorNode = nodes[i].previousSibling;
+    	var colorPicker = $(colorSelectHTML);
+		$(colorNode).replaceWith(colorPicker);
+		colorPicker.simplecolorpicker({picker:true});
+		
 		var e = $('<input type="text" class="edit-team-input" value="' + $(nodes[i]).text() + '"/><button class="button-xsmall pure-button team-remove-button">&#xd7;</button>');
 		if (i == 0) {
         	firstElement = e;
     	}
 		$(nodes[i]).replaceWith(e);
+		
+		
     }
     
     $('#teams').append('<button id="team-add" class="pure-button">Add team</button>');
