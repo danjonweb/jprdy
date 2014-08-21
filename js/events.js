445a51b2ede5d56f38153b7230061969e24c357f
danjonweb <dan.j.weber@gmail.com>
2014-08-21 10:33:23 -0700

    team edit fix

--- js/events.js
+++ js/events.js
@@ -2,7 +2,7 @@ $('#content').on('click','.question',function(){
 	var qid = this.getAttribute('id');
 	var q = getQuestionWithID(QUESTIONS,qid);
 	var qElement = this;
-    var questionValue = parseInt(qElement.textContent);
+    var questionValue = parseInt(q.value);
     
     $('#question').html('<h2>' + q.topic + ' for ' + q.value + '</h2><div id="question-inner">' + q.text + q.options + '</div>' + '<form class="pure-form"><input type="text" id="question-response" autocomplete="off">\n<button id="question-submit" class="pure-button pure-button-primary" qid="' + qid + '">OK</button><div id="answer">Correct response: ' + q.answer + '</div></form>');
     $('#fade').show();
@@ -26,7 +26,7 @@ $('#content').on('click','.question',function(){
             var response = document.getElementById('question-response').value;
             if (response.toLowerCase() == q.answer.toLowerCase()) {
                 $('#question').animate({backgroundColor: '#4CD964'}, 200);
-                teams[currentTeam].score = parseInt(teams[currentTeam].score) + questionValue;
+                TEAMS[currentTeam].score = parseInt(TEAMS[currentTeam].score) + questionValue;
             } else {
                 $('#question').animate({backgroundColor: '#FF2D55'}, 200);
                 $('#answer').css('display', 'inline-block');
@@ -42,7 +42,7 @@ $('#content').on('click','.question',function(){
                 $(this).off();
     
                 currentTeam++;
-                if (currentTeam >= teams.length) {
+                if (currentTeam >= TEAMS.length) {
                     currentTeam = 0;
                 }
                 showTeams();
@@ -64,66 +64,41 @@ $('#level-select').change(function(){
     window.location.href = updateQueryString('level', valueSelected);
 });
 
-$('#edit-teams').on('click',function(){
-	var colorSelectHTML = '<select name="color"><option value="#ac725e">#ac725e</option><option value="#d06b64">#d06b64</option><option value="#f83a22">#f83a22</option><option value="#fa573c">#fa573c</option><option value="#ff7537">#ff7537</option><option value="#ffad46">#ffad46</option><option value="#42d692">#42d692</option><option value="#16a765">#16a765</option><option value="#7bd148">#7bd148</option><option value="#b3dc6c">#b3dc6c</option><option value="#fbe983">#fbe983</option><option value="#fad165">#fad165</option><option value="#92e1c0">#92e1c0</option><option value="#9fe1e7">#9fe1e7</option><option value="#9fc6e7">#9fc6e7</option><option value="#4986e7">#4986e7</option><option value="#9a9cff">#9a9cff</option><option value="#b99aff">#b99aff</option><option value="#c2c2c2">#c2c2c2</option><option value="#cabdbf">#cabdbf</option><option value="#cca6ac">#cca6ac</option><option value="#f691b2">#f691b2</option><option value="#cd74e6">#cd74e6</option><option value="#a47ae2">#a47ae2</option></select>';
-    var nodes = document.querySelectorAll('.team-name');
-    var firstElement;
-    for (var i = 0; i < nodes.length; i++) {
-    	var colorNode = nodes[i].previousSibling;
-    	var colorPicker = $(colorSelectHTML);
-		$(colorNode).replaceWith(colorPicker);
-		colorPicker.simplecolorpicker({picker:true});
-		
-		var e = $('<input type="text" class="edit-team-input" value="' + $(nodes[i]).text() + '"/><button class="button-xsmall pure-button team-remove-button">&#xd7;</button>');
-		if (i == 0) {
-        	firstElement = e;
-    	}
-		$(nodes[i]).replaceWith(e);
-		
-		
-    }
-    
-    $('#teams').append('<button id="team-add" class="pure-button">Add team</button>');
-    $('#teams').append('<button id="team-done" class="pure-button">Done</button>');
-    $('#team-add').click(function(){
-    	var color = Colors.random();
-    	// Add a space between words
-    	var name = color.name.replace(/([a-z])([A-Z])/g, '$1 $2');
-    	var html = $('<li><span class="team-color" style="background-color:'+color.hex+';"></span>');
-        var e = $('<input type="text" class="edit-team-input" value="'+name+' Team"/>');
-        html.append(e);
-        html.append('<button class="button-xsmall pure-button team-remove-button">&#xd7;</button>');
-        html.append('<span class="team-score">0</span></li>');
-        $('ul#team-list').append(html);
-        setTimeout(function(){e.select();}, 50);
-        return false;
-    });
-    
-    $('#team-done').click(function(){
-    	teams = [];
-    	var nodes = document.querySelectorAll('.edit-team-input');
-        for (var i = 0; i < nodes.length; i++) {
-        	var Team = {};
-        	Team.name = nodes[i].value;
-        	Team.color = nodes[i].previousSibling.style.backgroundColor;
-        	Team.score = nodes[i].nextSibling.nextSibling.textContent;
-        	teams.push(Team);
-			$(nodes[i]).replaceWith($('<span class="team-name">'+Team.name+'</span>'));
-        }
-        $('#team-add').remove();
-        $('#team-done').remove();
-        $('.team-remove-button').remove();
-        if (currentTeam >= teams.length) {
-	        currentTeam = 0;
-        }
-        showTeams();
-        return false;
-    });
-    
-    setTimeout(function(){firstElement.select();}, 50);
+$('#teams-edit').on('click',function(){
+    showTeams(true);
     return false;
 });
 
-$('#teams').on('click','.team-remove-button',function(){
+$('#teams').on('click','#team-add',function(){
+	var Team = {};
+    Team.name = 'New Team';
+    Team.color = 25;
+    Team.score = '0';
+    TEAMS.push(Team);
+    
+	showTeams(true);
+	
+    return false;
+});
+    
+$('#teams').on('click','#team-done',function(){
+	TEAMS = [];
+	var nodes = document.querySelectorAll('#teams input[type="text"]');
+    for (var i = 0; i < nodes.length; i++) {
+    	var Team = {};
+    	Team.name = nodes[i].value;
+    	Team.color = nodes[i].previousSibling.previousSibling.selectedIndex;
+    	//console.log(nodes[i].previousSibling.previousSibling.selectedIndex);
+    	Team.score = nodes[i].nextSibling.nextSibling.textContent;
+    	TEAMS.push(Team);
+    }
+    if (currentTeam >= TEAMS.length) {
+        currentTeam = 0;
+    }
+    showTeams();
+    return false;
+});
+
+$('#teams').on('click','button.team-remove',function(){
 	$(this.parentNode).remove();
 });
